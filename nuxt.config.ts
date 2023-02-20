0c60e1b3e45b5fc7024393cf1a1218bb4fa044ba
dongkunshan <windwithfo@yeah.net>
2023-02-20 17:08:40 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,15 @@
-import { defineNuxtConfig } from 'nuxt'
-
 export default defineNuxtConfig({
   typescript: {
     shim: false
   },
   components: true,
   // Global page headers: https://go.nuxtjs.dev/config-head
-  meta: {
-    charset: 'utf-8',
-    viewport: 'width=device-width, initial-scale=1',
-  },
+  app: {
+    head: {
+      meta: [
+        { name: 'charset', content: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ]
+    }
+  }
 })
